921186cb6fd1c1d1f2662f9eea7935fbac7c1a93
KocAlex <alex.koca@alessandrinimainardi.edu.it>
2023-12-13 13:47:18 +0100

    err

--- calcolatrice_server.py
+++ calcolatrice_server.py
@@ -9,17 +9,17 @@ BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(SERVER_IP, SERVER_PORT)
 while True:
-#Blocco 1 (Server)
-        primoNumero = float(input("Inserisci il primo numero: "))
-        operazione = input("Inserisci l'operazione (simbolo)")
-        secondoNumero = float(input("Inserisci il secondo numero: "))
-        messaggio = {"primoNumero":primoNumero,
-                    "operazione":operazione,
-                    "secondoNumero":secondoNumero}
-        messaggio = json.dumps(messaggio)
-        s.sendall(messaggio.encode("UTF-8"))
+    #Blocco 1 (Server)
+    primoNumero = float(input("Inserisci il primo numero: "))
+    operazione = input("Inserisci l'operazione (simbolo)")
+    secondoNumero = float(input("Inserisci il secondo numero: "))
+    messaggio = {"primoNumero":primoNumero,
+                "operazione":operazione,
+                "secondoNumero":secondoNumero}
+    messaggio = json.dumps(messaggio)
+    s.sendall(messaggio.encode("UTF-8"))
 
-        #Ricevo il risultato
-        data = s.recv(1024)
-        print("Risultato: ", data.decode())
+    #Ricevo il risultato
+    data = s.recv(1024)
+    print("Risultato: ", data.decode())
 
