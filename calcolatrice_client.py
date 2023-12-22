0e710c6e6e2728c32528ae342641b54f44e82341
KocAlex <alex.koca@alessandrinimainardi.edu.it>
2023-12-22 09:10:19 +0100

    Fix

--- calcolatrice_client.py
+++ calcolatrice_client.py
@@ -1,35 +1,35 @@
-import socket, json
+import socket
+import json
 
 SERVER_IP = "127.0.0.1"
-SERVER_PORT = 5006 #???
+SERVER_PORT = 5005
 BUFFER_SIZE = 1024
 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((SERVER_IP, SERVER_PORT))
-
-#Blocco 2 (Cliente)
-#???
 while True:
-    #Ricevo i dati
-    data = s.recv(1024)
-    if not data:
-        break
-    data = data.decode()
-    data = json.loads(data)
-    primoNumero = data["primoNumero"]
-    operazione = data["operazione"]
-    secondoNumero = data["secondoNumero"]
+    #Blocco 1 (Server)
+    primoNumero = float(input("Inserisci il primo numero: "))
+    operazione = input("Inserisci l'operazione (simbolo)")
+    secondoNumero = float(input("Inserisci il secondo numero: "))
+    messaggio = {"primoNumero":primoNumero,
+                "operazione":operazione,
+                "secondoNumero":secondoNumero}
+    messaggio = json.dumps(messaggio)
+    s.sendto(messaggio.encode("UTF-8"), (SERVER_IP, SERVER_PORT))
 
-    #Calcolo il risultato e lo invio
-    risultato = 0
-    if operazione == "+":
-        risultato = primoNumero + secondoNumero
-    elif operazione == "-":
-        risultato = primoNumero - secondoNumero
-    elif operazione == "*":
-        risultato = primoNumero * secondoNumero
-    elif operazione == "/":
-        risultato = primoNumero / secondoNumero
-    elif operazione == "%":
-        risultato = primoNumero % secondoNumero
-    s.sendto(risultato, (SERVER_IP, SERVER_PORT))
+    #Ricevo il risultato
+    data = s.recv(1024)
+    print("Risultato: ", data.decode())
+
+
+
+
+
+#print("Server in attesa di messaggi...")
+
+#while True:
+#    data, addr = sock.recvfrom(BUFFER_SIZE)
+#    print(f"Messaggio ricevuto dal client {addr}: {data.decode()}")
+#    reply = "pong"
+#    sock.sendto(reply.encode(), addr)
--- calcolatrice_server.py
+++ calcolatrice_server.py
@@ -1,25 +1,35 @@
-import socket
-import json
+import socket, json
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 5005
 BUFFER_SIZE = 1024
 
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((SERVER_IP, SERVER_PORT))
+
+#Blocco 2
 while True:
-    #Blocco 1 (Server)
-    primoNumero = float(input("Inserisci il primo numero: "))
-    operazione = input("Inserisci l'operazione (simbolo)")
-    secondoNumero = float(input("Inserisci il secondo numero: "))
-    messaggio = {"primoNumero":primoNumero,
-                "operazione":operazione,
-                "secondoNumero":secondoNumero}
-    messaggio = json.dumps(messaggio)
-    s.sendall(messaggio.encode("UTF-8"))
+    #Ricevo i dati
+    data, addr = s.recvfrom(1024)
+    if not data:
+        break
+    data = data.decode()
+    data = json.loads(data)
+    primoNumero = data["primoNumero"]
+    operazione = data["operazione"]
+    secondoNumero = data["secondoNumero"]
 
-    #Ricevo il risultato
-    data = s.recv(1024)
-    print("Risultato: ", data.decode())
-
+    #Calcolo il risultato e lo invio
+    risultato = 0
+    if operazione == "+":
+        risultato = primoNumero + secondoNumero
+    elif operazione == "-":
+        risultato = primoNumero - secondoNumero
+    elif operazione == "*":
+        risultato = primoNumero * secondoNumero
+    elif operazione == "/":
+        risultato = primoNumero / secondoNumero
+    elif operazione == "%":
+        risultato = primoNumero % secondoNumero
+    
+    s.sendto(str(risultato).encode(), addr) #Invia a tutti i client
